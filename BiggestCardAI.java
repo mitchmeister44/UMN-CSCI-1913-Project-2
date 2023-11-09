1a09d5bfe8ce9a03811467196b09c4c9ea741c91
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-11-09 14:12:19 -0600

    Update BiggestCardAI.java

--- BiggestCardAI.java
+++ BiggestCardAI.java
@@ -7,30 +7,25 @@ package student;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class BiggestCardAI extends AI
-{
-    
+public class BiggestCardAI extends AI {
     @Override
     public Card getPlay(Hand hand, CardPile cardPile) {
-        Card[] validCards = new Card[hand.getSize()];
-        int maxRankNum = 1; 
-        Card returnCard = new Card(1,1);
-        for(int i = 0; i < hand.getSize(); i ++) {
-            if(cardPile.canPlay(hand.get(i)) == true) {
-                validCards[i] = hand.get(i);
-            }
-        }
-        for(int i = 0; i < validCards.length; i ++) {
-            if(validCards[i] == null) {
-                continue;
-            } else if(validCards[i].getRankNum() >= maxRankNum) {
-                returnCard = validCards[i];
-                maxRankNum = validCards[i].getRankNum();
+        Card returnCard = null;
+        Card referenceCard = null;
+        for (int i = 0; i < hand.getSize(); i ++) {
+            if (cardPile.canPlay(hand.get(i)) == true) {
+                referenceCard = hand.get(i);
+                if (returnCard == null) {
+                    returnCard = referenceCard; 
+                }
+                if (returnCard.getRankNum() < referenceCard.getRankNum()) {
+                    returnCard = referenceCard;
+                }
             }
         }
         return returnCard;
     }
-       
+
     @Override
     public String toString() {
         return "Biggest Card AI";
