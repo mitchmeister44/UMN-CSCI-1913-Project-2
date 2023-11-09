e960f0cdbb31cc59f1a6ee916fa3ddcd916f56d5
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-11-09 14:10:21 -0600

    Update AI.java

--- AI.java
+++ AI.java
@@ -6,18 +6,16 @@ package student;
  * @author Mitchell Smith
  * @version Written and compiled on 11/10/23
  */
-public class AI
-{
-
+public class AI {
     public Card getPlay(Hand hand, CardPile cardPile) {
-        for(int i = 0; i < hand.getSize(); i ++) {
-            if(cardPile.canPlay(hand.get(i)) == true) {
+        for (int i = 0; i < hand.getSize(); i++) {
+            if (cardPile.canPlay(hand.get(i)) == true) {
                 return hand.get(i);
             }
         }
         return null;
     }
-    
+
     public String toString() {
         return "Random Card AI";
     }
