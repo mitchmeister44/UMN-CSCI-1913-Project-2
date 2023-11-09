55a73d956d033e94c4d06546d5550fff278f1ddd
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-11-09 14:12:58 -0600

    Update CardPile.java

--- CardPile.java
+++ CardPile.java
@@ -5,15 +5,13 @@ package student;
  * @author Mitchell Smith
  * @version Written and compiled on 11/10/23
  */
-public class CardPile
-{
+public class CardPile {
     private Card topCard;
     private int numCards;
     /**
-     * Constructor for objects of class CardPile
+     * Constructor for objects of class CardPile.
      */
-    public CardPile(Card topCard)
-    {
+    public CardPile(Card topCard) {
         this.topCard = topCard;
         this.numCards = 1;
     }
@@ -22,34 +20,31 @@ public class CardPile
      * 
      */
     public boolean canPlay(Card card) {
-        if(card == null) {
+        if (card == null) {
             return false;
         }
-        if(card.getRankNum() > topCard.getRankNum()) {
+        if (card.getRankNum() >= topCard.getRankNum()) {
             return true;
         }
-        if(card.getRankNum() == topCard.getRankNum()) {
-            return true;
-        }
-        if(card.getSuitName() == topCard.getSuitName()) {
+        if (card.getSuitName() == topCard.getSuitName()) {
             return true;
         }
         return false;
     }
-    
+
     public void play(Card card) {
-        if(canPlay(card) == false) {
+        if (canPlay(card) == false) {
             System.out.println("Illegal move detected!");
         } else {
             numCards++;
             topCard = card;
         }
     }
-    
+
     public int getNumCards() {
         return numCards;
     }
-    
+
     public Card getTopCard() {
         return topCard;
     }
