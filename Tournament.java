7dcdcd8ffe438eee54804df0b15de06acce91aa3
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-11-09 14:14:18 -0600

    Update Tournament.java

--- Tournament.java
+++ Tournament.java
@@ -7,45 +7,37 @@ package student;
  * @author Mitchell Smith
  * @version Written and compiled on 11/10/23
  */
-public class Tournament
-{
-    public static void main (String[] args) {
-        AI r1 = new AI();
-        AI r2 = new AI();
-        AI s1 = new SmallestCardAI();
-        AI s2 = new SmallestCardAI();
-        AI b1 = new BiggestCardAI();
-        AI b2 = new BiggestCardAI();
-        
-        System.out.println(new UnoWarMatch(r1, r2).playGame());
-        
-        /*
-        UnoWarMatch match = new UnoWarMatch(r1, r2);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(r1, s1);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
+public class Tournament {
+    public static void main(String[] args) {
+        AI r = new AI();
+        AI s = new SmallestCardAI();
+        AI b = new BiggestCardAI();
 
-        match = new UnoWarMatch(r1, b1);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(s1, r1);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(s1, s2);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(s1, b1);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(b1, r1);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(b1, s1);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        
-        match = new UnoWarMatch(b1, b2);
-        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(2));
-        */
+        UnoWarMatch match = new UnoWarMatch(r, r);
+        System.out.println("Random Card AI vs. Random Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(r, s);
+        System.out.println("Random Card AI vs. Smallest AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(r, b);
+        System.out.println("Random Card AI vs. Biggest Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(s, r);
+        System.out.println("Smallest Card AI vs. Random Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(s, s);
+        System.out.println("Smallest Card AI vs. Smallest Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(s, b);
+        System.out.println("Smallest Card AI vs. Biggest Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(b, r);
+        System.out.println("Biggest Card AI vs. Random Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(b, s);
+        System.out.println("Biggest Card AI vs. Smallest Card AI winRate: " + match.winRate(1000));
+
+        match = new UnoWarMatch(b, b);
+        System.out.println("Biggest Card AI vs. Biggest Card AI winRate: " + match.winRate(1000));
     }
 }
